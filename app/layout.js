004c8e79691bbd2e323f23245e2de64261627264
codertoro <2082245876@qq.com>
2023-05-12 22:20:36 +0800

    analytics2

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,15 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
  
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'CoderToro',
+  description: 'You finally found me.',
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -16,17 +25,10 @@ export default function RootLayout({ children }) {
   );
 }
 
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'CoderToro',
-  description: 'You finally found me.',
-}
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="ch">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
-}
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="ch">
+//       <body className={inter.className}>{children}</body>
+//     </html>
+//   )
+// }
